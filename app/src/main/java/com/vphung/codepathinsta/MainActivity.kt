5ed07ae3ced4f429df0e3b437f813e029045087f
YourUsernamegit config --global user.name ioksing <nbaqq44@gmail.com>
2022-04-01 19:53:55 -0700

    Initial commit

---
+++ app/src/main/java/com/vphung/codepathinsta/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.vphung.codepathinsta
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.parse.*
+import com.vphung.codepathinsta.fragments.ComposeFragment
+import com.vphung.codepathinsta.fragments.FeedFragment
+import com.vphung.codepathinsta.fragments.ProfileFragment
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            item ->
+
+            var fragmentToShow: Fragment? = null
+            when (item.itemId) {
+                R.id.action_home -> {
+                    fragmentToShow = FeedFragment()
+
+                }
+                R.id.action_compose -> {
+                    fragmentToShow = ComposeFragment()
+
+                }
+                R.id.action_profile -> {
+                    fragmentToShow = ProfileFragment()
+
+                }
+
+                R.id.action_logout -> {
+                    ParseUser.logOut()
+                    goToLoginActivity()
+
+                }
+
+            }
+            if (fragmentToShow != null) {
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+
+            true
+        }
+
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
+
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+}
---
+++ app/src/main/java/com/vphung/codepathinsta/fragments/FeedFragment.kt
@@ -0,0 +1,91 @@
+package com.vphung.codepathinsta.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
+import com.vphung.codepathinsta.MainActivity
+import com.vphung.codepathinsta.Post
+import com.vphung.codepathinsta.PostAdapter
+import com.vphung.codepathinsta.R
+
+open class FeedFragment : Fragment() {
+
+    lateinit var postsRecyclerView: RecyclerView
+
+    lateinit var adapter: PostAdapter
+    lateinit var swipeContainer: SwipeRefreshLayout
+
+    var allPosts: ArrayList<Post> = ArrayList()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        swipeContainer = view.findViewById(R.id.swipeContainer)
+        swipeContainer.setOnRefreshListener {
+            Log.i(TAG, "Refreshing feed")
+            queryPosts()
+        }
+
+        swipeContainer.setColorSchemeResources(
+            android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light
+        )
+
+        postsRecyclerView = view.findViewById(R.id.postRecyclerView)
+        adapter = PostAdapter(requireContext(), allPosts)
+        postsRecyclerView.adapter = adapter
+
+        postsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        queryPosts()
+    }
+
+    open fun queryPosts() {
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.addDescendingOrder("createdAt")
+        query.setLimit(20)
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null) {
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG, "Post:" + post.getDescription() + " , username:" + post.getUser()?.username)
+                        }
+                        adapter.clear()
+                        allPosts.addAll(posts)
+                        adapter.notifyDataSetChanged()
+                        swipeContainer.setRefreshing(false)
+
+                    }
+                }
+            }
+        })
+    }
+
+    companion object {
+        const val TAG = "Feed Fragment"
+    }
+
+}
